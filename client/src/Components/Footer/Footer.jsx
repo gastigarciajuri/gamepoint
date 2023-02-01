a7ebba5939761a39b2602a95fd0c5c5ad23feef4
MariaBarrios8 <96894586+MariaBarrios8@users.noreply.github.com>
2023-02-01 18:24:49 -0300

    add footeer

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export default function Footer() {
+
+    return (
+        <div class='container-fluid'>
+            <section>
+                <div>
+                    <span>Get connected with us on social networks:</span>
+                </div>
+
+                <div>
+                    <a>
+                        
+                    </a>
+                </div>
+            </section>
+        </div>
+    )
+}

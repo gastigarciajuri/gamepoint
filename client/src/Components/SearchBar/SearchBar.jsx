a24a3e5a2a9da09714a79e188d6d44c79008e605
VicLara22 <lara.vic.moyano@gmail.com>
2023-02-02 15:03:32 -0300

    Config FormContac-SearchBar,CSS NavBar

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,30 +1,35 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import './SearchBar.css'
+import { getNameGame } from "../../Redux/action";
 
+export default function SearchBar() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
 
-export default function SearchBar () {
-    const dispatch = useDispatch();
-    const [name, setName] = useState('');
+  function handleInputChange(e) {
+    e.preventDefault();
+    setName(e.target.value.toLowerCase());
+  }
 
-
-    function handleInputChange(e) {
+  function handleSubmit(e) {
         e.preventDefault();
-        setName(e.target.value.toLowerCase());
-    }
-
-    /* function handleSubmit(e) {
-        e.preventDefault();
-        dispatch(getNameVideojuegos(name))
+        dispatch(getNameGame(name))
         setName('')
 
-    } */
-return (
-    <div>
-        <input type='text'
-        placeholder='Buscar...'
-        onChange={(e) => handleInputChange(e)} />
-        <button type='submit' /* onClick={(e) => handleSubmit(e)} */><SearchIcon/></button>
-    </div> 
-)
+    }
+  return (
+    <div className="searchbar">
+      <button type="submit"  onClick={(e) => handleSubmit(e)} className="button">
+        <SearchIcon className='iconS' />
+      </button>
+      <input
+        type="text"
+        placeholder="  Buscar..."
+        onChange={(e) => handleInputChange(e)}
+        className='input'
+      />
+    </div>
+  );
 }
--- client/src/Redux/action.js
+++ client/src/Redux/action.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-export const GET_ALL_VIDEOJUEGOS = 'GET_ALL_VIDEOJUEGOS';
+export const GET_ALL_VIDEOGAME = 'GET_ALL_VIDEOGAME';
+export const GET_NAME = 'GET_NAME';
 
 export const getAllVideojuegos = () => {
 	return async function (dispatch) {
 		try {
-			const json = await axios.get('http://localhost:3001/videojuegos');
+			const json = await axios.get('http://localhost:3001/videogame');
 			return dispatch({
-				type: GET_ALL_VIDEOJUEGOS,
+				type: GET_ALL_VIDEOGAME,
 				payload: json.data,
 			});
         } catch (error) {
@@ -15,3 +16,17 @@ export const getAllVideojuegos = () => {
 		}
 	};
 };
+
+export const getNameGame =(name) => {
+      return async function(dispatch){
+		try{
+			const json = await axios.get('http://localhost:3001/videogame?name=' + name)
+			return dispatch({
+				type: GET_NAME,
+				payload: json.data
+			})
+		} catch (error) {
+            console.log(error)
+		}
+	  }
+}
